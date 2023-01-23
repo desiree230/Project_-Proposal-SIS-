dacddad97ce30bc79e0b498546826108b9f67c87
desiree230 <120376381+desiree230@users.noreply.github.com>
2023-01-23 17:05:21 +0800

    Add files via upload

---
+++ URANZA_ Initial Code.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main ()
+
+{
+	cout << "************************************************************************************************************************\n";
+	cout << "                                               STUDENT INFORMATION SYSTEM\n";
+	cout << "                                                 WELCOME TO DESIREE SIS\n";
+	cout << "************************************************************************************************************************\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "                                                Select your Destination:\n                                                         ";
+	cout << "\n";
+	cout << "            STUDENTS:";
+	cout << "\n Press 1 == Sign-up New Student";
+	cout << "\n Press 2 == To View Records";
+	cout << "\n------------------------------------------------------------------------------------------------------------------------";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n          TEACHERS:";
+	cout << "\n Press 3 == Edit Records";
+	cout << "\n Press 4 == To Delete Records";
+	cout << "\n------------------------------------------------------------------------------------------------------------------------";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n Press 5 == Exit the program";
+	cout << "\n";
+	cout << "\nSelect your option:";
+
+}
