77e402114d035e5588c4316c7f69948194483e5f
desiree230 <120376381+desiree230@users.noreply.github.com>
2023-02-20 20:53:55 +0800

    Add files via upload

---
+++ URANZA FINAL INITIAL CODE/uranza initial code.cpp
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <iostream>
+
+using namespace std;
+
+int main()
+
+{
+	cout << "************************************************************************************************************************\n";
+	cout << "                                               STUDENT INFORMATION SYSTEM\n";
+	cout << "                                                 WELCOME TO DESIREE SIS\n";
+	cout << "************************************************************************************************************************\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n";
+	cout << "                                                Select your Destination:\n                                                         ";
+	cout << "\n";
+	cout << "            STUDENTS:";
+	cout << "\n          \t\t\t Press 1 == Sign-up New Student";
+	cout << "\n          \t\t\t Press 2 == To View Records";
+	cout << "\n------------------------------------------------------------------------------------------------------------------------";
+	cout << "\n";
+	cout << "\n";
+	cout << "            TEACHERS:";
+	cout << "\n          \t\t\t Press 3 == Edit Records";
+	cout << "\n          \t\t\t Press 4 == To Delete Records";
+	cout << "\n------------------------------------------------------------------------------------------------------------------------";
+	cout << "\n";
+	cout << "\n";
+	cout << "\n          \t\t\t Press 5 == View all records";
+	cout << "\n          \t\t\t Press 6 == Exit the Program";
+	cout << "\n";
+	cout << "\nSelect your option:";
+}
